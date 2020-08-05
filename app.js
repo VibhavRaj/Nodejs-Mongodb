5473c4f2de327420d9c2faa7c3abd5c074d18f97
unknown <vibhavraj88@gmail.com>
2020-08-05 23:52:22 +0530

    Added Cors

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Cors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Cotent-Type,Accept,Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 // Routes which handles the request
 app.use("/list", alllist);
 
