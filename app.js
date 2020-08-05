e0f49f9b9ce8377f7a308d82e52b77d9a8259371
unknown <vibhavraj88@gmail.com>
2020-08-05 19:59:23 +0530

    Added error handling to valid or unvalid redirect routes

--- app.js
+++ app.js
@@ -8,4 +8,20 @@ app.use(morgan("dev"));
 // Routes which handles the request
 app.use("/list", alllist);
 
+// Error Handling
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
 module.exports = app;
